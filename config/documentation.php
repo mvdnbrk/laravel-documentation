e460756b0eadc1ccbbeee217c079f127adc75460
Mark van den Broek <mvdnbrk@gmail.com>
2020-04-07 21:48:40 +0200

    Add docs

--- config/documentation.php
+++ config/documentation.php
@@ -2,12 +2,35 @@
 
 return [
 
+    /*
+    |--------------------------------------------------------------------------
+    | Documentation versions.
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify all published versions of your documentation
+    | and which version may serve as the default or latest version.
+    |
+    */
+
     'versions' => [
         'default' => '',
         'published' => [
             //
         ],
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Pages.
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify which page may be served a the default page.
+    | The index page is where you have of the "table of contents"
+    | for your documentation. This page is excluded from viewing
+    | by default. You may also specify additional pages to be
+    | excluded from viewing.
+    |
+    */
 
     'pages' => [
         'default' => 'installation',
